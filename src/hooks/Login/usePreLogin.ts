393fd95291f2145b3211e5031b20372289d169e7
chessmankir <skornkir2@gmail.com>
2026-04-14 17:43:53 +0300

    login android

---
+++ src/hooks/Login/usePreLogin.ts
@@ -0,0 +1,27 @@
+import {useRouter, useSegments} from "expo-router";
+import {useAuthStore} from "@/src/store/authStore";
+import {useEffect} from "react";
+
+export function useLogin(){
+    const router = useRouter();
+    const segments = useSegments();
+    const {isAuth, isLoading, restoreAuth} = useAuthStore();
+
+    useEffect(() => {
+        restoreAuth();
+    },[]);
+
+    useEffect(() => {
+        if (isLoading) return;
+
+        const inTabsGroup = segments[0] === "(tabs)";
+
+        if (!isAuth && inTabsGroup) {
+            router.replace('/login');
+        }
+
+        if(isAuth && !inTabsGroup){
+            router.replace('/(tabs)/players');
+        }
+    }, []);
+}
