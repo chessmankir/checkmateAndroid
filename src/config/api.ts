316f46b96bf1f5a49d47bbfc5e9eae8a646b9277
chessmankir <skornkir2@gmail.com>
2026-05-07 16:34:49 +0300

    fix android

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,2 +1,8 @@
-export const BASE_URL =
-    process.env.EXPO_PUBLIC_API_URL || "https://checkmatepubg.ru";
+export const BASE_URL = process.env.EXPO_PUBLIC_API_URL ||
+    "http://89.125.25.205:4000";
+
+/*export const BASE_URL =
+    "http://checkmatepubg.ru";*/
+
+/*
+process.env.EXPO_PUBLIC_API_URL || */
